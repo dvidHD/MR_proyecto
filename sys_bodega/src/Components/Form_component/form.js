754aab001e55aab7390335e21b2fbfdca14665d2
AnaBetJim <anabetjim12@gmail.com>
2018-11-11 18:28:26 -0600

    Vista formulario

--- sys_bodega/src/Components/Form_component/form.js
+++ sys_bodega/src/Components/Form_component/form.js
@@ -1,44 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+import '../Form_component/form.css';
 
-import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-class form extends Component {
-  state= { }
+export default class Example extends React.Component {
   render() {
-    return(
-    <Form>
-    <FormGroup>
-      <Label for="Nombre">Nombre </Label>
-      <Input type="nombre" name="nombre" id="nombreInstructor" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="EscCenter">Escuela/Center</Label>
-      <Input type="escuela" name="escuela" id="escuela" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="robot">Robot</Label>
-      <Input type="robot" name="robot" id="robot">
-        <option>Scribbler</option>
-        <option>Ev3</option>
-        <option>Frogduino</option>
-        <option>Arduino</option>
-        <option>Oruga</option>
-        <option>Raton </option>
-        <option>Sin Robots</option>
-      </Input>
-    </FormGroup>
-    
-    <FormGroup>
-      <Label for="consumibles">Consumibles</Label>
-      <Input type="consumibles" name="consumibles" id="consumible" placeholder="Foami,hojas,colores, etc ..."/>
-    </FormGroup>
-    
-    
-    
-    <Button>Enviar Solicitud</Button>
-  </Form>
-     ); }
+    return (
+      <Form>
+        <FormGroup >
+          <Label for="nombre"> Nombre </Label>
+          <Input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre" />
+        </FormGroup>
+        <FormGroup >
+          <Label for="datosCurso"> Curso y hora </Label>
+          <Input type="text" name="datosCurso" id="datosCurso" placeholder="Escuela/Center 00:00" />
+        </FormGroup>
+        <form class="listaRobots">
+          <Label for="robotSelect">Robots </Label>
+          <select id="robots" name="robots">
+          <option value="sc">Screbbler</option>
+          <option value="ev">Ev3</option>
+    n     <option value="frog">Forgduino</option>
+    n     <option value="ard">Arduino</option>
+          </select>
+        </form>
+        <FormGroup >
+        <form class="cantidad ">
+          <Label for="cantidad"> Cantidad </Label>
+          <Input type="text" name="cantidad" id="cantidad" placeholder="0" />
+          
+        </form>
+        </FormGroup>
+        <form class="matdidac">
+        <FormGroup>
+          <Label for="matdidac">Material Didactico </Label>
+          <Input type="textarea" name="matdidac" id="matdidac" placeholder="Foami,Memorama,Tijeras, etc ..."/>
+        </FormGroup></form>
+        <fom class="compu">
+        <FormGroup tag="fieldset">
+          <legend>Computadoras</legend>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="radio1" />{' '}
+              001
+            </Label>
+          </FormGroup>
+          <fom></fom>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="radio1" />{' '}
+              002
+            </Label>
+          </FormGroup>
+          <FormGroup check disabled>
+            <Label check>
+              <Input type="radio" name="radio1" disabled />{' '}
+             003
+            </Label>
+          </FormGroup>
+        </FormGroup>
+        </fom>
+        <Button>Confirmar Solicitud</Button>
+      
+      </Form>
+    );
+  }
 }
-
-export default form;
